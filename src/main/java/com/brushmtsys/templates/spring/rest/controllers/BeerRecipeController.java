6d2f804665385c34701ee65beab3f4ad3ef01f58
Justin Fleck <jfleck1@gmail.com>
2016-10-19 11:40:19 -0400

    Add stub json scheme endpoint

--- src/main/java/com/brushmtsys/templates/spring/rest/controllers/BeerRecipeController.java
+++ src/main/java/com/brushmtsys/templates/spring/rest/controllers/BeerRecipeController.java
@@ -2,7 +2,12 @@ package com.brushmtsys.templates.spring.rest.controllers;
 
 import com.brushmtsys.templates.spring.rest.model.BeerRecipe;
 import com.brushmtsys.templates.spring.rest.services.BeerRecipeService;
+import com.brushmtsys.templates.spring.rest.services.FileSystemBeerRecipeService;
+import com.fasterxml.jackson.module.jsonSchema.JsonSchema;
+import com.fasterxml.jackson.module.jsonSchema.JsonSchemaGenerator;
+import com.fasterxml.jackson.module.jsonSchema.factories.JsonSchemaFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,13 +19,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class BeerRecipeController {
 
     @Autowired
+    @Qualifier("fileSystemBeerRecipeService")
     private BeerRecipeService beerRecipeService;
 
-    @RequestMapping("/beer")
+    @RequestMapping("/BeerRecipe")
     @ResponseBody
     BeerRecipe getBeerRecipe(String name) {
         return beerRecipeService.read(name);
     }
 
-
+    @RequestMapping("/BeerRecipe/schema")
+    @ResponseBody
+    JsonSchema getBeerRecipeSchema(String name) {
+//        JsonSchema result = null;
+//        JsonSchemaGenerator generator = JsonSchemaGenerator;
+//
+//
+        return null;
+    }
 }
